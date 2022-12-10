e68745032523515064db8814e09d359e27a9fe0f
matty <mattybusscher@gmail.com>
2022-12-09 21:26:05 -0500

    init

---
+++ vite-portfolio/src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Social Media Site',
+        imgUrl: '',
+        stack: ['React','Express','MongoDB', 'Node'],
+        link: 'https://fkcodes.com',
+    },
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['React','Tailwind','Vite'],
+        link: 'https://fkcodes.com',
+    },
+    {
+        title: 'Ecom Site',
+        imgUrl: '',
+        stack: ['React','CSS'],
+        link: 'https://fkcodes.com',
+    },
+]
---
+++ vite-portfolio/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+    "./src/components/*.{jsx,js}"
+],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['inter', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
