e05306271cf0ecd26d0c1c039e87d1374f4b329f
matty <mattybusscher@gmail.com>
2022-12-10 19:22:54 -0500

    Timeline&Contactform

--- vite-portfolio/src/App.jsx
+++ vite-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 import Portfolio from "./components/Portfolio"
 import Timeline from "./components/Timeline"
+import Title from "./components/Title";
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Intro/>
       <Portfolio/>
       <Timeline/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
