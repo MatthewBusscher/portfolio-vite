5850927473b7c092978d1e07b8518c29fc9e79a8
matty <mattybusscher@gmail.com>
2022-12-14 18:34:35 -0500

    prod ready

--- vite-portfolio/src/data/portfolio.js
+++ vite-portfolio/src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Social Media Site',
-        imgUrl: './src/assets/Social.jpg',
+        imgUrl: '/assets/Social.jpg',
         stack: ['React','Express','MongoDB', 'Node'],
         link: '',
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: './src/assets/portfolio.jpg',
+        imgUrl: '/assets/portfolio.jpg',
         stack: ['React','Tailwind','Vite'],
         link: '',
     },
     {
         title: 'Ecom Site',
-        imgUrl: './src/assets/ecom.jpg',
+        imgUrl: '/assets/ecom.jpg',
         stack: ['React','CSS'],
         link: '',
     },
